a3644ccb4a519a22821ae391ba45c2369cee1c3d
vietsaclo <vietsaclo@gmail.com>
2020-05-11 09:48:45 +0700

    hoan thanh giao dien

---
+++ C#/GameCoCaRo/GameCoCaRo/GameCaro.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace GameCoCaRo
+{
+    partial class GameCaro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbNguoiChoiA = new System.Windows.Forms.TextBox();
+            this.tbNguoiChoiB = new System.Windows.Forms.TextBox();
+            this.btnBatDau = new System.Windows.Forms.Button();
+            this.btnChoiLai = new System.Windows.Forms.Button();
+            this.pnBanCo = new System.Windows.Forms.Panel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nguoi Choi A:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nguoi Choi B:";
+            // 
+            // tbNguoiChoiA
+            // 
+            this.tbNguoiChoiA.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbNguoiChoiA.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.tbNguoiChoiA.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbNguoiChoiA.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.tbNguoiChoiA.Location = new System.Drawing.Point(110, 17);
+            this.tbNguoiChoiA.Name = "tbNguoiChoiA";
+            this.tbNguoiChoiA.Size = new System.Drawing.Size(329, 20);
+            this.tbNguoiChoiA.TabIndex = 1;
+            this.tbNguoiChoiA.Text = "Nguoi Choi A";
+            // 
+            // tbNguoiChoiB
+            // 
+            this.tbNguoiChoiB.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbNguoiChoiB.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.tbNguoiChoiB.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbNguoiChoiB.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.tbNguoiChoiB.Location = new System.Drawing.Point(110, 42);
+            this.tbNguoiChoiB.Name = "tbNguoiChoiB";
+            this.tbNguoiChoiB.Size = new System.Drawing.Size(329, 20);
+            this.tbNguoiChoiB.TabIndex = 3;
+            this.tbNguoiChoiB.Text = "Nguoi Choi B";
+            // 
+            // btnBatDau
+            // 
+            this.btnBatDau.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBatDau.Location = new System.Drawing.Point(445, 17);
+            this.btnBatDau.Name = "btnBatDau";
+            this.btnBatDau.Size = new System.Drawing.Size(75, 45);
+            this.btnBatDau.TabIndex = 4;
+            this.btnBatDau.Text = "Bat Dau";
+            this.btnBatDau.UseVisualStyleBackColor = true;
+            this.btnBatDau.Click += new System.EventHandler(this.btnBatDau_Click);
+            // 
+            // btnChoiLai
+            // 
+            this.btnChoiLai.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnChoiLai.Enabled = false;
+            this.btnChoiLai.Location = new System.Drawing.Point(526, 17);
+            this.btnChoiLai.Name = "btnChoiLai";
+            this.btnChoiLai.Size = new System.Drawing.Size(75, 45);
+            this.btnChoiLai.TabIndex = 5;
+            this.btnChoiLai.Text = "Choi Lai";
+            this.btnChoiLai.UseVisualStyleBackColor = true;
+            this.btnChoiLai.Click += new System.EventHandler(this.btnChoiLai_Click);
+            // 
+            // pnBanCo
+            // 
+            this.pnBanCo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnBanCo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnBanCo.Location = new System.Drawing.Point(24, 81);
+            this.pnBanCo.Name = "pnBanCo";
+            this.pnBanCo.Size = new System.Drawing.Size(573, 282);
+            this.pnBanCo.TabIndex = 6;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(24, 370);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(573, 23);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // GameCaro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 404);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pnBanCo);
+            this.Controls.Add(this.btnChoiLai);
+            this.Controls.Add(this.btnBatDau);
+            this.Controls.Add(this.tbNguoiChoiB);
+            this.Controls.Add(this.tbNguoiChoiA);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "GameCaro";
+            this.Text = "Game Caro";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbNguoiChoiA;
+        private System.Windows.Forms.TextBox tbNguoiChoiB;
+        private System.Windows.Forms.Button btnBatDau;
+        private System.Windows.Forms.Button btnChoiLai;
+        private System.Windows.Forms.Panel pnBanCo;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ C#/GameCoCaRo/GameCoCaRo/GameCaro.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameCoCaRo
+{
+    public partial class GameCaro : Form
+    {
+        //Variables
+        private Button[,] banCo;
+        private int soO;
+        private bool isNguoiChoiA;
+
+        public GameCaro()
+        {
+            InitializeComponent();
+            initMyComponent();
+        }
+
+        private void initMyComponent()
+        {
+            isNguoiChoiA = true;
+            soO = 10;
+            banCo = new Button[soO, soO];
+        }
+
+        private void taiGiaoDien()
+        {
+            Button btn;
+            for (int i = 0; i < soO; i++)
+            {
+                for (int j = 0; j < soO; j++)
+                {
+                    btn = createButton(i, j);
+                    banCo[i, j] = btn;
+                    pnBanCo.Controls.Add(btn);
+                }
+            }
+        }
+
+        private Button createButton(int i, int j)
+        {
+            Button btn = new Button();
+            btn.Tag = i + "," + j;
+            btn.Width = btn.Height = 50;
+            btn.Cursor = Cursors.Hand;
+            btn.Font = new Font(btn.Font.FontFamily, 20f);
+            btn.Location = new Point(j * (btn.Width + 3), i * (btn.Height + 3));
+            btn.Click += btn_Click;
+            return btn;
+        }
+
+        void btn_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (isNguoiChoiA)
+            {
+                btn.Text = "X";
+                btn.ForeColor = Color.Red;
+            }
+            else
+            {
+                btn.Text = "O";
+                btn.ForeColor = Color.Blue;
+            }
+            isNguoiChoiA = !isNguoiChoiA;
+            setNguoiChoi();
+        }
+
+        private void btnBatDau_Click(object sender, EventArgs e)
+        {
+            taiGiaoDien();
+            setNguoiChoi();
+            btnBatDau.Enabled = false;
+            btnChoiLai.Enabled = true;
+        }
+
+        private void btnChoiLai_Click(object sender, EventArgs e)
+        {
+            btnChoiLai.Enabled = false;
+            btnBatDau.Enabled = true;
+        }
+
+        private void setNguoiChoiA()
+        {
+            tbNguoiChoiA.BackColor = Color.Brown;
+            tbNguoiChoiA.Font = new Font(tbNguoiChoiA.Font, FontStyle.Bold);
+            tbNguoiChoiA.ForeColor = Color.White;
+
+            tbNguoiChoiB.BackColor = Color.White;
+            tbNguoiChoiB.Font = new Font(tbNguoiChoiA.Font, FontStyle.Regular);
+            tbNguoiChoiB.ForeColor = Color.Black;
+        }
+
+        private void setNguoiChoiB()
+        {
+            tbNguoiChoiB.BackColor = Color.DarkBlue;
+            tbNguoiChoiB.Font = new Font(tbNguoiChoiA.Font, FontStyle.Bold);
+            tbNguoiChoiB.ForeColor = Color.White;
+
+            tbNguoiChoiA.BackColor = Color.White;
+            tbNguoiChoiA.Font = new Font(tbNguoiChoiA.Font, FontStyle.Regular);
+            tbNguoiChoiA.ForeColor = Color.Black;
+        }
+
+        private void setNguoiChoi()
+        {
+            if (isNguoiChoiA)
+                setNguoiChoiA();
+            else
+                setNguoiChoiB();
+        }
+    }
+}
